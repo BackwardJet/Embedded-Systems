33e0d3e5e5131d1b3e4428c5e0164aa4dc93b80c
tejvuligonda <tejvuligonda@gmail.com>
2016-04-27 18:25:55 -0700

    Initial commit with Project 1 & 2 working

---
+++ Project1_Blink-LED/Project1_Blink-LED/main.c
@@ -0,0 +1,18 @@
+/*
+ * Project1_Blink-LED.c
+ *
+ * Created: 4/27/2016 5:57:46 PM
+ * Author : Tej Vuligonda
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ Project3_Music-Player/Project3_Music-Player/main.c
@@ -0,0 +1,19 @@
+/*
+ * Project3_Music-Player.c
+ *
+ * Created: 4/27/2016 5:20:06 PM
+ * Author : Tej Vuligonda
+ */ 
+
+#include <avr/io.h>
+#include "avr.h"
+#include "lcd.h"
+
+int main(void)
+{
+    /* Replace with your application code */
+
+	ini_lcd();
+	puts_lcd2("PROJECT 3");
+}
+
