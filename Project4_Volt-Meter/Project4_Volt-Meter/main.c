63076268bc4cde2d9c15db3c15fcd6e989d1e50d
tejvuligonda <tejvuligonda@gmail.com>
2016-05-11 17:19:22 -0700

    Set-up Project 4

---
+++ Project4_Volt-Meter/Project4_Volt-Meter/main.c
@@ -0,0 +1,17 @@
+/*
+ * Project4_Volt-Meter.c
+ *
+ * Created: 5/11/2016 5:16:28 PM
+ * Author : Tej Vuligonda
+ */ 
+
+#include <avr/io.h>
+#include "lcd.h"
+
+
+int main(void)
+{
+	ini_lcd();
+	puts_lcd2("PROJECT 4");
+}
+
