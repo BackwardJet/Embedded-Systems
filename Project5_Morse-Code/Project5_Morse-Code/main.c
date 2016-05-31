1294f0a955fe4b3998f54aca4e8f9c0cc38c987a
tejvuligonda <tejvuligonda@gmail.com>
2016-05-30 19:38:43 -0700

    Initial commit for Project 5

---
+++ Project5_Morse-Code/Project5_Morse-Code/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ * Created: 5/30/2016 7:34:50 PM
+ *  Author: Administrator
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+	while(1)
+	{
+		//TODO:: Please write your application code
+	}
+}
